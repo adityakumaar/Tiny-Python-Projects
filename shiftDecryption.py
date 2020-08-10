c1f0b1925d61f6f70e5dff26c5e69eaf02ee507d
Aditya Kumar <10adityakumar98@gmail.com>
2020-08-10 22:12:33 +0530

    Decrypting strings using Shift Decryption

---
+++ shiftDecryption.py
@@ -0,0 +1,18 @@
+import string
+
+alphabets = "abcdefghijklmnopqrstuvwxyz"
+#The sentence is required to be in lower case and without spaces
+sentence = input("Enter the string to decrypt (lowercase): ")
+
+#The shift value can be either negative or positive
+#(opposite of encryption shift value)
+val = int(input("Enter Shift Value: "))
+
+def startEncryption(string, shift_val = val):
+    encrypted_string = ""
+    for i in string:
+        index = alphabets.index(i)
+        shifted_index = (index + shift_val) % len(alphabets)
+        encrypted_string += alphabets[shifted_index]
+    return encrypted_string
+print(startEncryption(sentence))
