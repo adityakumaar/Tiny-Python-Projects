a007905051f03cca5ddad06750fa0ef21ad9f5a5
Aditya Kumar <10adityakumar98@gmail.com>
2020-08-14 22:38:56 +0530

    play mp3 files with python

---
+++ musicPlayer.py
@@ -0,0 +1,20 @@
+from pygame import mixer
+
+mixer.init()
+
+mixer.music.load("music.mp3")
+mixer.music.set_volume(0.7)
+mixer.music.play()
+
+while True:
+    print("Press 'p' to pause, 'r' to resume: ")
+    print("Press 'e' to exit the music player: ")
+    query = input(">>> ")
+
+    if query == 'p':
+        mixer.music.pause()
+    elif query == 'r':
+        mixer.music.unpause()
+    elif query == 'e':
+        mixer.music.stop()
+        break
