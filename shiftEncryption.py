da17135c52510589399a4676a589783c5794ce00
Aditya Kumar <10adityakumar98@gmail.com>
2020-10-01 23:11:05 +0530

    Update shiftEncryption.py

--- shiftEncryption.py
+++ shiftEncryption.py
@@ -7,6 +7,8 @@ sentence = input("Enter the string to encrypt (lowercase): ")
 #The shift value can be either negative or positive
 val = int(input("Enter Shift Value: "))
 
+
+
 def startEncryption(string, shift_val = val):
     encrypted_string = ""
     for i in string:
